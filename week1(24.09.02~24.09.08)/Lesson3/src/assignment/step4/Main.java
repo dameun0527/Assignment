9c4ba8bcd7c396eee073b5a5102fe78d234190b9
류지수 <jisuryu0527@gmail.com>
2024-09-07 14:28:28 +0900

    Add: Personal_Assignment

--- week1(24.09.02~24.09.08)/Lesson3/src/assignment/step4/Main.java
+++ week1(24.09.02~24.09.08)/Lesson3/src/assignment/step4/Main.java
@@ -2,10 +2,18 @@ package assignment.step4;
 
 
 public class Main {
-    public  void main () {
+    public static void main(String[] args){
+
         Calculator calculator = new Calculator(new AddOperation());
         System.out.println(calculator.calculate(10,20));
+
+        calculator.setOperation(new SubstractOperation());
+        System.out.println(calculator.calculate(10,20));
+
         calculator.setOperation(new MultiplyOperation());
+        System.out.println(calculator.calculate(10,20));
+
+        calculator.setOperation(new DivideOperation());
         System.out.println(calculator.calculate(10,20));
 
     }
